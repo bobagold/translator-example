94f36e5fb8ad168c158339e94afeff76f1f5bfd6
Vladimir Goldobin <vgold@xiag.ch>
2012-11-15 16:36:22 +0700

    update example to use updated translator api

--- public/index.php
+++ public/index.php
@@ -11,7 +11,11 @@ echo $app->injectAtClientSide(basename(__FILE__), 'ru');
 function translator() {
     return new Translator\Application(
         't.translator-example.local',
-        new Translator\CouchDbDriver(),
+        new Translator\CouchDbStorage(new \CouchDB\Connection(
+            new \CouchDB\Http\LoggingClient(
+                new \CouchDB\Http\StreamClient('localhost', 5984)
+            )
+        )),
         array_key_exists('t', $_GET) ?
                 Translator\Application::TRANSLATE_ON: Translator\Application::TRANSLATE_OFF
     );
